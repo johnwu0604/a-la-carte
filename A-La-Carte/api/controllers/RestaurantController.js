f7b66b8fed362e8fed4fb126e3eeaf8f70bd773b
John Wu <john.wu@mail.mcgill.ca>
2016-09-17 01:28:29 -0400

    Created object

---
+++ A-La-Carte/api/controllers/RestaurantController.js
@@ -0,0 +1,11 @@
+/**
+ * RestaurantController
+ *
+ * @description :: Server-side logic for managing restaurants
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ A-La-Carte/api/models/Restaurant.js
@@ -0,0 +1,75 @@
+/**
+ * Restaurant.js
+ *
+ * @description :: This represents the entities of a restaurant object
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    id:{
+      type:"string",
+      required:true,
+      minLength: 1
+    },
+    name:{
+      type:"string",
+      required:true,
+      minLength: 1
+    },
+    latitude:{
+      type:"string",
+      required:false,
+      minLength: 1
+    },
+    longitude:{
+      type:"string",
+      required:false,
+      minLength: 1
+    },
+    rating:{
+      type:"string",
+      required:false,
+      minLength: 1
+    },
+    cuisine:{
+      type:"string",
+      required:false,
+      minLength: 1
+    },
+    address:{
+      type:"string",
+      required:false,
+      minLength: 1
+    },
+    city:{
+      type:"string",
+      required:false,
+      minLength: 1
+    },
+    province:{
+      type:"string",
+      required:false,
+      minLength: 1
+    },
+    image:{
+      type:"string",
+      required:false,
+      minLength: 1
+    },
+    url:{
+      type:"string",
+      required:false,
+      minLength: 1
+    },
+    number:{
+      type:"string",
+      required:false,
+      minLength: 1
+    }
+
+  }
+};
+
